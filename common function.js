7e86c0389fc4dc83941c037b75cffc82b5d3685e
hasanMahmud743 <h.mahmud.7430@gmail.com>
2022-08-22 16:25:12 +0600

    event handler added

--- common function.js
+++ common function.js
@@ -26,3 +26,25 @@ function setValue(value, id){
     const getValue = document.getElementById(id)
     getValue.innerText = value
 }
+
+// function for add event listner
+
+function addEventListener(fieldID, listPlayerName){
+    document.getElementById(fieldID).addEventListener('click', function(){
+
+        const button = document.getElementById(fieldID)
+        button.setAttribute('disabled', true) 
+    
+        const playerName = document.getElementById(listPlayerName)
+        const innerText = playerName.innerText
+
+        const playerList = document.getElementById('player-list')
+    
+        const creat = document.createElement('li')
+        creat.innerText = innerText
+        playerList.appendChild(creat)
+    
+    
+    
+    })
+}
--- player.js
+++ player.js
@@ -1,5 +1,6 @@
 document.getElementById('calculate-button').addEventListener('click', function(){
     const playerPrice = getValue('player-amount')
+
     if(isNaN(playerPrice)){
         alert('insert a number')
         return
@@ -23,9 +24,38 @@ document.getElementById('total-calculate').addEventListener('click', function(){
     }
 
     const finalTotal = totalPlayerCost + managerCost + coachCost
-
-    
     setValue(finalTotal, 'final-total' )
 
-   
 })
+
+
+
+
+// document.getElementById('click-one').addEventListener('click', function(){
+
+//     const button = document.getElementById('click-one')
+//     button.setAttribute('disabled', true) 
+
+//     const playerName = document.getElementById('player-name')
+//     const innerText = playerName.innerText
+//     const playerList = document.getElementById('player-list')
+
+//     const creat = document.createElement('li')
+//     creat.innerText = innerText
+//     playerList.appendChild(creat)
+
+
+
+// })
+
+addEventListener('first-button', 'first-player')
+addEventListener('sec-button', 'sec-player')
+addEventListener('third-button', 'third-player')
+addEventListener('fourth-button', 'fourth-player')
+addEventListener('fifth-button', 'fifth-player')
+addEventListener('sixth-button', 'sixth-player')
+
+
+
+const a = document.getElementsByClassName('player-list').length
+console.log(a)
